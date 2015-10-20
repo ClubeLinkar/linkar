7c93e8db66b56f73b7c5a36ad41e162657186901
Lennon Jesus <lennon.jesus@gmail.com>
2015-10-20 18:51:48 -0200

    botando angular em tudo que da

--- src/main/java/br/com/clubelinkar/controller/StoreController.java
+++ src/main/java/br/com/clubelinkar/controller/StoreController.java
@@ -1,6 +1,8 @@
 package br.com.clubelinkar.controller;
 
 import br.com.clubelinkar.domain.Store;
+import br.com.clubelinkar.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class StoreController {
 
+    @Autowired
+    private StoreRepository storeRepository;
+
     @RequestMapping(value = "/store/new", method = RequestMethod.GET)
     public String create(Model model) {
         model.addAttribute("store", new Store());
 
         return "store/form";
+    }
+
+    @RequestMapping(value = "/store/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        return "store/list";
     }
 
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,23 +1,30 @@
 angular.module("linkar", [])
     .controller("ProductController", function ($scope, $http) {
 
+        $scope.product = {};
+        $scope.mensagem = "";
+
+        $scope.salvar = function() {
+
+            $http.post("/api/v1/product", $scope.product).success(function () {
+                $scope.mensagem = "product adicionada com sucesso!";
+            });
+        }
 
         $http.get("/api/v1/product").success(function (data) {
             $scope.productList = data;
         });
 
-        $scope.alert = function() {
-            alert("BLAH!");
-        }
+        $http.get("/api/v1/store").success(function (data) {
+            $scope.storeList = data;
+        });
 
-})
-    .controller("StoreController", function ($scope, $http) {
+    }).controller("StoreController", function ($scope, $http) {
 
         $scope.store = {};
         $scope.mensagem = "";
 
         $scope.salvar = function() {
-            console.log(data);
 
             $http.post("/api/v1/store", $scope.store).success(function () {
                 $scope.mensagem = "Loja adicionada com sucesso!";
@@ -25,5 +32,10 @@ angular.module("linkar", [])
         }
 
 
-    }
-);
+        $http.get('/api/v1/store').success(function (data) {
+            $scope.storeList = data;
+        });
+
+
+    })
+;
