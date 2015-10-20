9a58724da9efc5d0ec5ee16b0255945498497f07
Lennon Jesus <lennon.jesus@gmail.com>
2015-10-20 19:30:52 -0200

    adicionando domain customer

---
+++ src/main/java/br/com/clubelinkar/domain/Customer.java
@@ -0,0 +1,82 @@
+package br.com.clubelinkar.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by lennon.lima on 20/10/2015.
+ */
+@Document
+public class Customer {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String uf;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
