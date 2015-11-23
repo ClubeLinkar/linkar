a0e0fdf6ec6c39bcac660966ae61df3f4d124b2c
Lennon Jesus <lennon.jesus@gmail.com>
2015-11-23 19:39:46 -0200

    comecando os rascunhos de user com angular

---
+++ linkar-frontend/app/user/user.controller.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict'
+
+  angular.module('linkar').controller('User', UserController);
+
+  function UserController($scope, User) {
+
+    console.log('user::controller:init');
+
+    User.query(function (data) {
+      console.log("user::controller:list");
+      $scope.userList = data;
+    });
+
+    this.save = function () {
+      User.save($scope.user, function (data) {
+        console.log("user::controller:save");
+        $scope.message = "Success!";
+      });
+    }
+  }
+
+})();
