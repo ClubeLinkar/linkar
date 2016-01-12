7660502ce9939df9e15648e5b6a3476543cced0e
Lennon Jesus <lennon.jesus@gmail.com>
2016-01-11 23:29:49 -0200

    comecando a dar cara no adm

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -18,7 +18,43 @@ angular.module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', '
             url: "/minor",
             templateUrl: "app/minor/minor.html",
             data: { pageTitle: 'Example view' }
-        });
+        })
+
+        .state('product', {
+            abstract: true,
+            url: "/product",
+            templateUrl: "components/common/content.html"
+        })
+        .state('product.main', {
+            url: "/main",
+            templateUrl: "app/product/main.html",
+            data: { pageTitle: 'Produtos' }
+        })
+
+        .state('store', {
+            abstract: true,
+            url: "/store",
+            templateUrl: "components/common/content.html"
+        })
+        .state('store.main', {
+            url: "/main",
+            templateUrl: "app/store/main.html",
+            data: { pageTitle: 'Lojas' }
+        })
+
+        .state('user', {
+            abstract: true,
+            url: "/user",
+            templateUrl: "components/common/content.html"
+        })
+        .state('user.main', {
+            url: "/main",
+            templateUrl: "app/user/main.html",
+            data: { pageTitle: 'Usuários' }
+        })
+
+
+        ;
 
     $urlRouterProvider.otherwise('/index/main');
   })
