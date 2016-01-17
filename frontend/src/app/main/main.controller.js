d44d0a6b28bd78313066a11bb76e1beb4a04b916
Lennon Jesus <lennon.jesus@gmail.com>
2016-01-17 11:12:56 -0200

    removendo arquivos de exemplo inspirai

--- frontend/src/app/main/main.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('linkar')
-  .controller('MainCtrl', function ($scope) {
-
-    this.helloText = 'Linkar';
-    this.descriptionText = 'Sua loja colaborativa';
-
-  });
